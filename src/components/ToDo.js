5d05b7250dee4a33396e5dd983d9741a7ac79f7a
vasu2008 <80173814+vasu2008@users.noreply.github.com>
2023-10-12 15:59:43 +0530

    Add files via upload

---
+++ src/components/ToDo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import {BiEdit} from "react-icons/bi"
+import {AiFillDelete} from "react-icons/ai"
+
+const ToDo = ({text, updateMode, deleteToDo}) => {
+  return (
+    <div className="todo">
+        <div className="text">{text}</div>
+        <div className="icon">
+            <BiEdit className='icon' onClick={updateMode}/>
+            <AiFillDelete className='icon' onClick={deleteToDo}/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default ToDo
